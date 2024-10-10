089ef3f6aefd6c62ddeb29a56c0b27fde3df6fea
8okqb5s7 <tiagosilva.profile@gmail.com>
2024-10-10 19:33:56 -0300

    [ADS-0000] Atualizando.

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
 
 /*======================== typed js ========================*/
 const typed = new Typed('.multiple-text',{
-    strings: ['Seguro Automotivo', 'Planos de Saúde', 'Planos Odontologicos'],
+    strings: ['', '', ''],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 100,
